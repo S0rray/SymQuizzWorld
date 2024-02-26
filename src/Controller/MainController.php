93040f44652799a6816b4720911b06a05986184c
S0rray <oliv33@laposte.net>
2024-02-26 22:44:16 +0100

    transfert 26-02

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\DifficultiesRepository;
+use App\Repository\ThemesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(): Response
+    public function index(ThemesRepository $themesRepository, DifficultiesRepository $difficultiesRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'themes' => $themesRepository->getName(),
+            'difficulties' => $difficultiesRepository->getName()
         ]);
     }
 }
